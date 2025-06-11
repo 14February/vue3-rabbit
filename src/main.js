9b7fcbe81940e12def01d83960a4a0b04640bc37
yurui <ttttttesla0214@gmail.com>
2025-06-12 00:10:02 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import {getCategoryAPI} from '@/apis/testApi'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+getCategoryAPI().then(res => {
+    console.log(res)
+})
